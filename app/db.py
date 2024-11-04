20a2b8ecddaf977ab257e734c8251fc26c11d12d
Eugen Hoxha <eugenhoxha0@gmail.com>
2024-11-04 14:34:57 +0200

    Improve db.py documentation and contex by including docstrings for each function

--- app/db.py
+++ app/db.py
@@ -1,25 +1,37 @@
+"""
+This module provides utility functions for managing database connections and sessions.
+"""
+import sqlmodel
 from contextlib import contextmanager
 from typing import Generator
 
-import sqlmodel
 from sqlalchemy import create_engine, Engine
 from sqlmodel import Session
 
+DATABASE_URL = "sqlite:///backend.db"
 
 def get_engine() -> Engine:
-    return create_engine("sqlite:///backend.db", echo=True)
-
+    """
+    Creates a SQLAlchemy engine.
+    """
+    return create_engine(DATABASE_URL, echo=True)
 
 def get_db() -> Generator[Session, None, None]:
     """
-    Retrieves new SQLAlchemy Session from connection pool
+    Retrieves a new SQLAlchemy Session from the connection pool.
     :yield: SQLAlchemy Session
     """
-    with sqlmodel.Session(get_engine()) as session:
+    engine = get_engine()
+    with Session(engine) as session:
         yield session
-
 
 @contextmanager
 def get_database_session() -> Generator[Session, None, None]:
-    with sqlmodel.Session(get_engine()) as session:
+    """
+    Context manager for database session
+    :yield: SQLAlchemy Session
+    """
+    engine = get_engine()
+    with Session(engine) as session:
         yield session
+        
